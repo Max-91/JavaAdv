3f043d35a5d1918be685629b7bcadb5a0eb9c927
Max <Maximkot@mail.ru>
2020-09-23 02:17:22 +0300

    Homework1

--- src/Homework1/Cat.java
+++ src/Homework1/Cat.java
@@ -1,4 +1,31 @@
 package Homework1;
 
-public class Cat {
+public class Cat implements RunJump {
+    private int limRun;
+    private int limJump;
+
+    public Cat(int limRun, int limJump) {
+        this.limRun = limRun;
+        this.limJump = limJump;
+    }
+
+    @Override
+    public boolean Run(int lenght) {
+        if (lenght < limRun) {
+            System.out.println("Кошка пробежала " + lenght + " м (предел " + limRun + " м)");
+            return true;
+        }
+        System.out.println("Кошка не смогла пробежать " + lenght + " м (предел " + limRun + " м)");
+        return false;
+    }
+
+    @Override
+    public boolean Jump(int height) {
+        if (height < limJump) {
+            System.out.println("Кошка перепрыгнула " + height + " см (предел " + limRun + " см)");
+            return true;
+        }
+        System.out.println("Кошка не смогла перепругнуть " + height + " см (предел " + limRun + " см)");
+        return false;
+    }
 }
--- src/Homework1/ObsWall.java
+++ src/Homework1/ObsWall.java
@@ -1,4 +1,13 @@
 package Homework1;
 
-public class ObsWall {
+public class ObsWall implements Obstacle {
+    private int obsHeight;
+
+    public ObsWall(int obsHeight) {
+        this.obsHeight = obsHeight;
+    }
+
+    public boolean work(RunJump rj) {
+        return rj.Jump(obsHeight);
+    }
 }
