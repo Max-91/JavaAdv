02e9e1ae5276064ad49e6506635a8d95e5120f9f
Max <Maximkot@mail.ru>
2020-09-30 16:43:44 +0300

    Homework2

--- src/Homework2/MyArraySizeException.java
+++ src/Homework2/MyArraySizeException.java
@@ -1,4 +1,20 @@
 package Homework2;
 
 public class MyArraySizeException extends RuntimeException {
+    private int row; // Номер строки в которой проблема
+
+    public MyArraySizeException(int row) {
+        this.row = row;
+    }
+
+    public MyArraySizeException(String message, int row) {
+        super(message);
+        this.row = row;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+
 }
