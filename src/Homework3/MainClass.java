43a418a9daa6684bf9af54a0b334d6409e72b14b
Max <Maximkot@mail.ru>
2020-10-03 17:22:41 +0300

    Homework3

--- src/Homework3/MainClass.java
+++ src/Homework3/MainClass.java
@@ -1,4 +1,8 @@
 package Homework3;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 1. Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся). Найти и
 вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
@@ -10,5 +14,27 @@ package Homework3;
 однофамильцев), тогда при запросе такой фамилии должны выводиться все телефоны.
  */
 public class MainClass {
+    public static void main(String[] args) {
+        // Задание 1
+        String[] words = {
+                "самолет", "ребенок", "вертолет", "мир", "море", "берег",
+                "солнце", "мир", "марс", "земля", "солнце", "море", "солнце"};
+        Map<String, Integer> mapWords = new HashMap<>();
+        // Заполнение map
+        for (String word : words) {
+            Integer count = mapWords.getOrDefault(word, 0);
+            mapWords.put(word, count + 1);
+        }
+        System.out.println(mapWords);
+        // Задание 2
+        PhoneBook phoneBook= new PhoneBook();
+        phoneBook.Add("Иванов",new String[]{"123","84991733533"});
+        phoneBook.Add("Петров",new String[]{"+79991112233"});
+        phoneBook.Add("Сидоров",new String[]{"03"});
 
+        //System.out.println(phoneBook.toString()); // Проверка - отображение всей тел.книги
+
+        phoneBook.Get("Иванов");
+    }
 }
+
--- src/Homework3/PhoneBook.java
+++ src/Homework3/PhoneBook.java
@@ -1,4 +1,36 @@
 package Homework3;
 
+import java.util.*;
+
 public class PhoneBook {
+    private Map<String, List<String>> pb = new HashMap<>();
+
+    public void Add(String name,String[] phone){
+        List<String> list = new LinkedList<>();
+        for (int i = 0; i < phone.length; i++) {
+            list.add(phone[i]);
+        }
+        pb.put(name, list);
+    }
+
+    public void Get(String name) {
+        List<String> list = new LinkedList<>();
+        list=pb.get(name);
+        if (list==null) {
+            System.out.println("Абонента "+name+" нет в телефонной книги");
+            return;
+        };
+        if (list.size()>1) {
+            System.out.println("Телефоны ("+list.size()+" шт.) абонента " + name + " : " + list);
+        }else{
+            System.out.println("Телефон абонента " + name + " : " + list);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneBook{" +
+                "pb=" + pb +
+                '}';
+    }
 }
