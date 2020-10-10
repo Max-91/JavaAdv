c7ea0d3b48598b1b05bc70e957d433b5fee45d99
Max <Maximkot@mail.ru>
2020-10-10 16:05:35 +0300

    Homework5-init

---
+++ src/Homework5/MainClass.java
@@ -0,0 +1,16 @@
+package Homework5;
+
+/*
+1) Создают одномерный длинный массив, например:
+static final int size = 10000000;
+static final int h = size / 2;
+float[] arr = new float[size];
+2) Заполняют этот массив единицами;
+3) Засекают время выполнения: long a = System.currentTimeMillis();
+4) Проходят по всему массиву и для каждой ячейки считают новое значение по формуле:
+arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+5) Проверяется время окончания метода System.currentTimeMillis();
+6) В консоль выводится время работы: System.out.println(System.currentTimeMillis() - a);
+ */
+public class MainClass {
+}
